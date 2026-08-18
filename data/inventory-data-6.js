f94710617a5b1aead3fbca82937b2e01b360ed33
Weymuth <149831932+Weymuth@users.noreply.github.com>
2026-08-18 09:43:50 -0400

    Add new VEX catalog items from complete list

---
+++ data/inventory-data-6.js
@@ -0,0 +1,15 @@
+window.INVENTORY_DATA=(window.INVENTORY_DATA||[]).concat([
+{"i":"P-000195","p":"VEX","n":"V5 Robot Battery Li-Ion","m":"VEX Robotics","x":"276-4811","c":"Power","g":"https://content.vexrobotics.com/docs/276-4811-v5-battery.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Current","iv":0},
+{"i":"P-000196","p":"VEX","n":"V5 Robot Battery Charger","m":"VEX Robotics","x":"276-4812","c":"Power","g":"https://content.vexrobotics.com/docs/276-4812-v5-charger.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Current","iv":0},
+{"i":"P-000197","p":"VEX","n":"V5 Smart Cable Stock (50 ft)","m":"VEX Robotics","x":"276-4815","c":"Electronics","g":"https://content.vexrobotics.com/docs/276-4815-v5-cable-stock.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Current","iv":0},
+{"i":"P-000198","p":"VEX","n":"V5 Smart Cable Crimping Tool","m":"VEX Robotics","x":"276-4832","c":"Tools","g":"https://content.vexrobotics.com/docs/276-4832-crimp-tool.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Current","iv":0},
+{"i":"P-000199","p":"VEX","n":"V5 System Bundle","m":"VEX Robotics","x":"276-7000","c":"Kits","g":"https://content.vexrobotics.com/docs/276-7000-v5-system-bundle.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Current","iv":0},
+{"i":"P-000200","p":"VEX","n":"V5 Competition Starter Kit","m":"VEX Robotics","x":"276-7010","c":"Kits","g":"https://content.vexrobotics.com/docs/276-7010-v5-starter-kit.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Current","iv":0},
+{"i":"P-000201","p":"VEX","n":"V5 Competition Super Kit","m":"VEX Robotics","x":"276-7020","c":"Kits","g":"https://content.vexrobotics.com/docs/276-7020-v5-super-kit.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Current","iv":0},
+{"i":"P-000202","p":"VEX","n":"VEX ARM Cortex-based Microcontroller","m":"VEX Robotics","x":"276-2194","c":"Electronics","g":"https://content.vexrobotics.com/docs/276-2194-cortex.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Retired","iv":0},
+{"i":"P-000203","p":"VEX","n":"VEXnet Joystick","m":"VEX Robotics","x":"276-2192","c":"Electronics","g":"https://content.vexrobotics.com/docs/276-2192-joystick.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Retired","iv":0},
+{"i":"P-000204","p":"VEX","n":"VEXnet Key 2.0","m":"VEX Robotics","x":"276-3245","c":"Electronics","g":"https://content.vexrobotics.com/docs/276-3245-vexnet-key.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Retired","iv":0},
+{"i":"P-000205","p":"VEX","n":"VEX 2-Wire Motor 393","m":"VEX Robotics","x":"276-2181","c":"Motion","g":"https://content.vexrobotics.com/docs/276-2181-motor-393.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Retired","iv":0},
+{"i":"P-000206","p":"VEX","n":"Motor Controller 29","m":"VEX Robotics","x":"276-2193","c":"Electronics","g":"https://content.vexrobotics.com/docs/276-2193-mc29.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Retired","iv":0},
+{"i":"P-000207","p":"VEX","n":"VEX Robot Battery NiMH 7.2V 3000mAh","m":"VEX Robotics","x":"276-2177","c":"Power","g":"https://content.vexrobotics.com/docs/276-2177-battery.jpg","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","s":"Retired","iv":0}
+]);
