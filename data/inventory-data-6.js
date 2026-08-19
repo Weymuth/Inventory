b9d0e5b169de46c4efdeba14ecf6c0b4d6ba2686
Weymuth <149831932+Weymuth@users.noreply.github.com>
2026-08-18 23:09:46 -0400

    Temporarily remove admin enhancement layer to restore card details

--- data/inventory-data-6.js
+++ data/inventory-data-6.js
@@ -1,1 +1,1 @@
-document.write('<script src="data/inventory-data-6-core.js"><\/script><script src="data/inventory-admin-editor.js?v=20260818-2306"><\/script>');
+document.write('<script src="data/inventory-data-6-core.js?v=20260818-2310"><\/script>');
