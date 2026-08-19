651341b4d26c5de626ab9d3a083dedf3037cd7de
Weymuth <149831932+Weymuth@users.noreply.github.com>
2026-08-18 22:50:34 -0400

    Load reliable card click fallback

--- data/inventory-data-6.js
+++ data/inventory-data-6.js
@@ -1,1 +1,1 @@
-document.write('<script src="data/inventory-data-6-core.js"><\/script><script src="data/inventory-admin-editor.js"><\/script>');
+document.write('<script src="data/inventory-data-6-core.js"><\/script><script src="data/inventory-admin-editor.js"><\/script><script src="data/card-click-fix.js"><\/script>');
