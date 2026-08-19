fc1e56acabebc052d875e4dc0a6484f895baf08c
Weymuth <149831932+Weymuth@users.noreply.github.com>
2026-08-19 08:50:18 -0400

    Load expanded VEX catalog data

--- data/inventory-data-6.js
+++ data/inventory-data-6.js
@@ -1,1 +1,1 @@
-document.write('<script src="data/inventory-data-6-core.js?v=20260818-2310"><\/script><script src="data/delete-card-simple.js?v=20260818-2313"><\/script>');
+document.write('<script src="data/inventory-data-6-core.js?v=20260818-2310"><\/script><script src="data/inventory-data-7.js?v=20260819-0842"><\/script><script src="data/delete-card-simple.js?v=20260818-2313"><\/script>');
