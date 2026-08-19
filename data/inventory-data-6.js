4f6619b376d34ba42ceb1b1bd35f3635c9117612
Weymuth <149831932+Weymuth@users.noreply.github.com>
2026-08-18 23:13:43 -0400

    Load simple delete-card helper

--- data/inventory-data-6.js
+++ data/inventory-data-6.js
@@ -1,1 +1,1 @@
-document.write('<script src="data/inventory-data-6-core.js?v=20260818-2310"><\/script>');
+document.write('<script src="data/inventory-data-6-core.js?v=20260818-2310"><\/script><script src="data/delete-card-simple.js?v=20260818-2313"><\/script>');
