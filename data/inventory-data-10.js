49e763080d6c78539f2951e97c38de1f78a3eb8a
Weymuth <149831932+Weymuth@users.noreply.github.com>
2026-08-19 09:14:51 -0400

    Add VEX chassis rail catalog records

---
+++ data/inventory-data-10.js
@@ -0,0 +1,4 @@
+window.INVENTORY_DATA=(window.INVENTORY_DATA||[]).concat([
+{"i":"P-000297","p":"VEX","n":"2x1x25 Steel Chassis Rail (4-pack)","m":"VEX Robotics","x":"275-1145","c":"Structural","g":"","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","ua":0,"ni":1,"sg":1},
+{"i":"P-000298","p":"VEX","n":"2x1x35 Steel Chassis Rail (4-pack)","m":"VEX Robotics","x":"275-1146","c":"Structural","g":"","r":0,"a":0,"t":0,"o":0,"z":0,"w":"","ua":0,"ni":1,"sg":1}
+]);
